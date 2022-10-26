6562117ab7f697f2a0bfe7140e3c1e8a9068dd90
IdirX69 <adil.idirr@gmail.com>
2022-10-26 13:44:20 +0200

    Search bar + filter and map with array

--- frontend/src/components/MainPage/Mainpage.jsx
+++ frontend/src/components/MainPage/Mainpage.jsx
@@ -8,14 +8,22 @@ import Searchbar from "./Searchbar";
 function Mainpage() {
   const [searchValue, setSearchValue] = useState("");
 
-  const [games] = useState(["Diablo", "Valorant", "Elden ring"]);
+  const results = {
+    applist: {
+      apps: [
+        { appid: 1, name: "Valorant" },
+        { appid: 2, name: "Fable" },
+        { appid: 3, name: "Elden ring" },
+      ],
+    },
+  };
   return (
     <>
       <Searchbar searchValue={searchValue} setSearchValue={setSearchValue} />
-      {games
-        .filter((game) => game.includes(searchValue))
+      {results.applist.apps
+        .filter((game) => game.name.includes(searchValue))
         .map((game) => (
-          <li>{game}</li>
+          <li>{game.name}</li>
         ))}
       <Navbar />
       <Novelties />
