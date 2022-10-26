fed2ca7c6a39cca01e249d6d204c8f71cb5aade9
Chloé Bidau <chloebidau@hotmail.fr>
2022-10-26 11:34:53 +0200

    addind routes

--- frontend/src/components/Game/ButtonClose.jsx
+++ frontend/src/components/Game/ButtonClose.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
 
 function ButtonClose() {
   // function to return to the main page. Need to make a Route to '/' and let click event to change the url
@@ -7,9 +8,18 @@ function ButtonClose() {
   };
 
   return (
-    <button type="button" onClick={closePage} className="buttonclose">
-      X
-    </button>
+    <>
+      <button type="button" onClick={closePage} className="buttonclose">
+        X
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Main Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
--- frontend/src/components/MainPage/Mainpage.jsx
+++ frontend/src/components/MainPage/Mainpage.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { BrowserRouter as Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Novelties from "./Novelties";
 import Quizz from "./Quizz/Quizz";
 import Ourlikes from "./Ourlikes";
+import Game from "../Game/Game";
 
 function Mainpage() {
   return (
     <>
       <Navbar />
       <Novelties />
+      <Link to="/novelties">Novelties</Link>
       <Quizz />
+      <Link to="/quizz">Quizz</Link>
       <Ourlikes />
+      <Link to="/likes">Likes</Link>
+      <Game />
+      <Link to="/game">Game</Link>
     </>
   );
 }
