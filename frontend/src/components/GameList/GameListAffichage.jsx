ec3d4105d468bd444ded478a8c2fa715fd3110d0
Sahrane_Guassemi <sahranedev@gmail.com>
2022-11-01 17:04:59 +0100

    props destrucration + data api extraction of filtred games + starting CSS on page filtred games

--- frontend/src/components/GameList/GameListAffichage.jsx
+++ frontend/src/components/GameList/GameListAffichage.jsx
@@ -1,18 +1,55 @@
 import React from "react";
 import "./gameListStyle.css";
+import "../SingleGame/singleGame.css";
+import GameGenreDetails from "../SingleGame/GameGenreDetails";
 
 function GameListAffichage({
   name,
   background_image: backgroundImage,
   metacritic,
+  released,
+  genres,
+
+  platforms: [
+    {
+      platform: { name: platformeName },
+    },
+  ],
+  /* tags: [{ name: tagName }], */
+
+  metacritic_url: metacriticUrl,
 }) {
+  const colorCritic = () => {
+    if (metacritic >= 75) {
+      return "metacriticGreen";
+    }
+    if (metacritic >= 40) {
+      return "metacriticYellow";
+    }
+    return "metacriticRed";
+  };
+
   return (
     <div className="gamePageContainer">
-      <h2>{name}</h2>
-      <img className="backgroundGameImage" src={backgroundImage} alt="jeu" />
-      <div className="fav-metacritic">
+      <div className="backgroundGameImage">
+        <img src={backgroundImage} alt="jeu" />
+      </div>
+      <div className="gameInfo">
+        <h2 className="gameName">{name} </h2>
+        <div className={colorCritic(metacritic)}>
+          <a href={metacriticUrl} target="_blank" rel="noreferrer">
+            {metacritic}
+          </a>
+        </div>{" "}
+        <div className="release">
+          <strong>Sortie : </strong>
+          {released}
+        </div>
+        <div className="gamePageDetails">
+          {genres ? <GameGenreDetails genres={genres} /> : null}
+        </div>
+        <div className="platform">{platformeName}</div>
         <div>❤️</div>
-        <div>Note : {metacritic}</div>
       </div>
     </div>
   );
