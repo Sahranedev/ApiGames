70f22c00201e630cc6eb1e9535d1645b823e030a
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-11-08 16:45:04 +0100

    Match between fetch for filtredlist by platforms - made some change about the title components

--- frontend/src/components/FiltredList/FiltredlList.jsx
+++ frontend/src/components/FiltredList/FiltredlList.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
+import GameListAffichage from "@components/GameList/GameListAffichage";
 
 function FiltredList() {
   const API_URL = "https://api.rawg.io/api/games";
   const API_KEY = "813e525c42c04986ac0747dddec96609";
+  const isMount = useRef(false);
   const { filtredList } = useParams();
   /* Transmets l'ID de la plateforme pour la donner au prochain fetch */
   const [filter, setFilter] = useState();
-  /* Transmets les informations filtrées */
-  /* const [filtredGames, setFiltredGames] = useState([]); */
+  /* Permet de stocker les jeux de la plateforme */
+  const [gamesFiltred, setGamesFiltred] = useState([]);
 
   // Requête pour récupérer les ID des plateformes
   const getPlatformsList = async () => {
@@ -21,57 +23,60 @@ function FiltredList() {
     // Pour chaque plateforme de la liste je vérifie si le nom est égal à la valeur du params.
     platformsList.forEach((platform) => {
       if (platform.slug === filtredList) {
-        /* console.log("c'est good"); */
         setFilter(platform.id);
-        /*  console.log(filter); */
         // Si ce n'est pas bon, puisqu'il existe des sous catégories (ex:PS5) je fais pose la condition
         // sur le sous-tableau afin de faire la même vérification.
       } else if (platform.slug !== filtredList) {
         platform.platforms.forEach((sousplatforms) => {
           if (sousplatforms.slug === filtredList) {
-            /*  console.log("OK 2"); */
             setFilter(sousplatforms.id);
-            /*  console.log(filter); */
             // Si vraiment rien ne se passe on renvoie null
           } else {
             null;
           }
         });
       } else {
-        /* console.log("après tout check non"); */
+        console.log("après tout check non");
       }
     });
   };
 
   // Requête pour récupérer les jeux en fonction de la plateforme
-  // On envoie les données dans le useState prévu à cet effet
   const getFiltredList = async () => {
     const response = await fetch(
       `${API_URL}?key=${API_KEY}&platforms=${filter}&ordering=-metacritic&page_size=5`
     );
-    const filtredGamesData = await response.json();
-    /* setFiltredGames(filtredGamesData); */
+    const gamesresult = await response.json();
+    setGamesFiltred(gamesresult.results);
+    console.log(gamesresult.results);
   };
 
+  // On utilise un useEffect basé sur l'évolution du filtredList pour réactualiser la requête
+  // quand on change de plateforme depuis le menu, vu qu'onn ne change pas de composant sinon ça ne recharge pas
   useEffect(() => {
     getPlatformsList();
-    getFiltredList();
+    console.log(filter);
   }, [filtredList]);
 
-  // pour chaque filter if  filter.name === filtredList
-  // if yes, then fetch the API with the filter.id
-  // if not, fetch the API with the filtredList
+  // On utilise un useEffect basé sur le montage du composant pour ne pas déclencher la requête du listing en début de chargement de page
+  useEffect(() => {
+    if (isMount.current) {
+      console.log(filter);
+      getFiltredList();
+    } else {
+      isMount.current = true;
+    }
+  }, [filter]);
 
   return (
     <div>
       <h2>Filtred games by "{filtredList}"</h2>
-      {/*  filtredGames && (
-      <div className="game-list">
-        {filtredGames.map((game) => (
-          <GameListAffichage key={game.id} {...game} />
-        ))}
-      </div>
-      ) */}
+
+      <h1>{filter}</h1>
+
+      {gamesFiltred?.map((game) => (
+        <GameListAffichage {...game} key={game.id} />
+      ))}
     </div>
   );
 }
