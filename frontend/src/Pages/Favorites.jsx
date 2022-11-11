596ca7cfa959d2a03e489f17bd56886640e48f6d
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-11-11 10:40:34 +0100

    First idea about Favorite page display

--- frontend/src/Pages/Favorites.jsx
+++ frontend/src/Pages/Favorites.jsx
@@ -1,7 +1,91 @@
 import React from "react";
+import IMGnotFound from "../images/IMGnotFound.png";
+import "../Styles/favorite.css";
 
 function Favorites() {
-  return <div>Favorites</div>;
+  return (
+    <div className="container p-md-5 favorite-container">
+      <h2 className="p-md-5">Favorites</h2>
+      <div className="container md-p-5 ">
+        <div className="row">
+          <div className="col">
+            <div className="game-list-favorite">
+              <div className="single-game">
+                <div className="row">
+                  <div className="col-md-2 col-sm-2">
+                    <img
+                      src={IMGnotFound}
+                      alt="user"
+                      className="game-photo-lg"
+                    />
+                  </div>
+
+                  <div className="col-md-7 col-sm-7 py-4">
+                    <h5>
+                      <a href="#" className="">
+                        Elden Ring
+                      </a>
+                    </h5>
+                  </div>
+                  <div className="col-md-3 col-sm-3 py-3">
+                    <button className="btn  pull-right">
+                      <div className="like text-center" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="single-game">
+                <div className="row">
+                  <div className="col-md-2 col-sm-2">
+                    <img
+                      src={IMGnotFound}
+                      alt="user"
+                      className="game-photo-lg"
+                    />
+                  </div>
+                  <div className="col-md-7 col-sm-7 py-4">
+                    <h5>
+                      <a href="#" className="profile-link">
+                        League of legends
+                      </a>
+                    </h5>
+                  </div>
+                  <div className="col-md-3  col-sm-3 py-3">
+                    <button className="btn  pull-right">
+                      <div className="like text-center" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="single-game">
+                <div className="row">
+                  <div className="col-md-2 col-sm-2">
+                    <img
+                      src={IMGnotFound}
+                      alt="user"
+                      className="game-photo-lg"
+                    />
+                  </div>
+                  <div className="col-md-7 col-sm-7 py-4">
+                    <h5>
+                      <a href="#" className="">
+                        Dark Souls : Remastered
+                      </a>
+                    </h5>
+                  </div>
+                  <div className="col-md-3 col-sm-3 py-3 ">
+                    <button className="btn  pull-right">
+                      <div className="like text-center" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Favorites;
