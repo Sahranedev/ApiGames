ab3888f254ecdc7e8c10a3fed40d8c8405495fc1
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-11-14 23:32:16 +0100

    Switch for a bulb lamp its so cute

--- frontend/src/components/GameGenreDisplay.jsx
+++ frontend/src/components/GameGenreDisplay.jsx
@@ -5,7 +5,7 @@ function GameGenreDisplay({ genres: genre }) {
     <>
       {/* On map la props pour permettre l'affichage de plusieurs genres si il y (sinon seul le premier genre du tableau apparaît) */}
 
-      {genre.slice(0, 3).map((el, i) => (
+      {genre.slice(0, 3).map((el) => (
         // eslint-disable-next-line react/no-array-index-key
 
         <div className="col-fluid">
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import "../Styles/navBar.css";
 import logoTest from "../images/LogoTest.png";
 
 function NavBar({ searchValue, setSearchValue, getGame, toggleTheme, theme }) {
+  const [bulb, setBulb] = useState(false);
+  const handleBulb = () => {
+    setBulb(!bulb);
+  };
+
   return (
     <nav className="navbar navbar-expand-md fixed-bottom sticky-md-top">
       <div className="container-fluid">
@@ -330,19 +336,39 @@ function NavBar({ searchValue, setSearchValue, getGame, toggleTheme, theme }) {
             {/* Toggle light mode or dark mode */}
 
             <li className="dropdown-item d-flex justify-content-end justify-content-md-start">
-              <div className="form-check mx-3 form-switch mx-md-5 my-md-2 d-flex justify-content-end justify-content-md-start toggle-btn">
-                <label
-                  className="form-check-label"
-                  htmlFor="flexSwitchCheckDefault"
-                >
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    onClick={toggleTheme}
-                    role="switch"
-                    id="flexSwitchCheckDefault"
-                  />
-                </label>
+              <div
+                className="form-check mx-3 form-switch mx-md-5 my-md-2 d-flex justify-content-end justify-content-md-start toggle-btn"
+                onClick={toggleTheme}
+                type="button"
+              >
+                {bulb ? (
+                  <svg
+                    onClick={handleBulb}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-lightbulb"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z" />
+                  </svg>
+                ) : (
+                  <svg
+                    onClick={handleBulb}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="black"
+                    className="bi bi-lightbulb-off"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.23 4.35A6.004 6.004 0 0 0 2 6c0 1.691.7 3.22 1.826 4.31.203.196.359.4.453.619l.762 1.769A.5.5 0 0 0 5.5 13a.5.5 0 0 0 0 1 .5.5 0 0 0 0 1l.224.447a1 1 0 0 0 .894.553h2.764a1 1 0 0 0 .894-.553L10.5 15a.5.5 0 0 0 0-1 .5.5 0 0 0 0-1 .5.5 0 0 0 .288-.091L9.878 12H5.83l-.632-1.467a2.954 2.954 0 0 0-.676-.941 4.984 4.984 0 0 1-1.455-4.405l-.837-.836zm1.588-2.653.708.707a5 5 0 0 1 7.07 7.07l.707.707a6 6 0 0 0-8.484-8.484zm-2.172-.051a.5.5 0 0 1 .708 0l12 12a.5.5 0 0 1-.708.708l-12-12a.5.5 0 0 1 0-.708z"
+                    />
+                  </svg>
+                )}
               </div>
             </li>
           </ul>
